53cb6339dc151969ba30db4292e911dbcc0d9f62
T3Marius <T3Marius@users.noreply.github.com>
2026-03-21 13:34:07 +0200

    fix: cookies error

--- ShopCore/src/ShopCore.cs
+++ ShopCore/src/ShopCore.cs
@@ -12,7 +12,7 @@ namespace ShopCore;
 
 [PluginMetadata(
     Id = "ShopCore",
-    Version = "v1.1.1-beta.v2",
+    Version = "v1.1.2",
     Name = "ShopCore",
     Author = "T3Marius",
     Description = "Core shop plugin exposing items and credits API."
